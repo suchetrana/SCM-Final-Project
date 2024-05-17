749d71c9102125331cf5a12d94d60c09faf35ad8
Suchet <suchetrana27@gmail.comgit config --global user.emailsuchetrana27@gmail.com>
2024-05-17 17:16:03 +0530

    adding food.js file

--- food.js
+++ food.js
@@ -28,3 +28,4 @@ function getRandomFoodPosition() {
   }
   return newFoodPosition;
 }
+
--- score.js
+++
@@ -1,6 +0,0 @@
-export function increment() {
-  const val = document.querySelector(".value");
-  let score = val.innerHTML;
-  score++;
-  val.innerHTML = score;
-}
